e969bca7ab5c3a36c5ad150dd7919af5db00b2bd
Tiburce Kouagou <kouagoutiburce@gmail.com>
2022-12-13 12:37:12 +0100

    Implémentation des méthodes match, add, getRoutes, getParams du router

--- core/Router.php
+++ core/Router.php
@@ -4,5 +4,58 @@
  * Controlleur Frontal
  * PHP version 8.1
  */
- class Router {  
+ class Router {
+    /**
+     * Routes de l'application
+     * (Table des routes)
+     *
+     * @var array
+     */
+    private $routes = [];
+
+    /**
+     * Paramètres de la route actuelle
+     *
+     * @var array
+     */
+    private $params = [];
+
+    /**
+     * Ajouter des routes à la table des routes
+     *
+     * @param string $url Route
+     * @param array $params Paramètres de la route
+     * @return void
+     */
+    public function add($url, $params) {
+        $this->routes[$url] = $params;
+    }
+    
+    /**
+     * Permet de faire correspondre la chaîne de requête
+     *@param string $url La chaine de requête à faire correspondre
+     * @return boolean
+     */
+    public function match($url) {
+        foreach($this->getRoutes() as $route => $param) {
+            if ($route === $url) {
+                $this->params[$url] = $param;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Retourne les routes de l'application
+     *
+     * @return array
+     */
+    public function getRoutes() {
+        return $this->routes;
+    }
+
+    public function getParams() {
+        return $this->params;
+    }
  }
--- public/index.php
+++ public/index.php
@@ -5,3 +5,16 @@ require "../core/Router.php";
 $router = new Router();
 $url = $_SERVER["QUERY_STRING"];
 echo "La chaine de requête est \"$url\"";
+
+$router->add("", ["controller" => "Home", "action" => "index"]);
+$router->add("posts", ["controller" => "Posts", "action" => "index"]);
+$router->add("posts/show", ["controller" => "Posts", "action" => "show"]);
+
+var_dump($router->getRoutes());
+
+
+if ($router->match($url)) {
+    var_dump($router->getParams());
+} else {
+    echo "Aucune route correspondant à \"$url\"";
+}
