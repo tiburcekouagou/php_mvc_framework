a302ee2dfa62a4abbeaf6bdcbd45f6f721d6583b
tiburekouagou <kouagoutiburce@gmail.com>
2023-01-05 07:16:04 +0100

    Séparation des données de connection à la BDD dans un fichier Config.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Core\Model;
+use Database\Config;
 
 
 class Post extends Model
@@ -13,13 +14,15 @@ class Post extends Model
    */
   public static function getAll()
   {
-    $host = "localhost";
-    $db_name = "php_mvc_framework";
-    $username = "root";
-    $password = "";
+    // $host = "localhost";
+    // $db_name = "php_mvc_framework";
+    // $username = "root";
+    // $password = "";
 
     try {
-      $db = new \PDO("mysql:host=$host;dbname=$db_name;charset=utf8;", $username, $password);
+      // $db = new \PDO("mysql:host=$host;dbname=$db_name;charset=utf8;", $username, $password);
+      $dsn = "mysql:host=" . Config::DB_HOST . ";dbname=" . Config::DB_NAME . ";charset=utf8;";
+      $db = new \PDO($dsn, Config::DB_USER, Config::DB_PASSWORD);
       $stmt = $db->query('SELECT id, title, content FROM posts ORDER BY created_at');
       $results = $stmt->fetchAll(\PDO::FETCH_ASSOC);
 
---
+++ database/Config.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database;
+
+/**
+ * Configurqtion de l'application
+ * 
+ * PHP version 5.4
+ */
+class Config {
+  /**
+   * Nom du serveur
+   * @var string
+   */
+  const DB_HOST = 'localhost';
+  /**
+   * Nom de la BDD
+   * @var string
+   */
+  const DB_NAME = 'php_mvc_framework';
+  /**
+   * Username de la BDD
+   * @var string
+   */
+  const DB_USER = 'root';
+  /**
+   * Mot de la passe de la BDD
+   * @var string
+   */
+  const DB_PASSWORD = '';
+}
